2ddfba5d6a79708762d468a960d7b7881877a0d5
hanifmt <hanifmtsabit@gmail.com>
2018-07-11 12:24:26 +0700

    update

---
+++ application/views/V_witel.php
@@ -0,0 +1,4 @@
+<h3>Nama Witel</h3>
+<input type="text" name="txtWitel" required="true">
+<button type="reset">Cancel</button>
+<button type="submit">Input Data</button>
--- application/views/tampilan/V_combine.php
+++ application/views/tampilan/V_combine.php
@@ -1,6 +1,9 @@
 <?php 
 
 include('V_header.php');
+
+$this->load->view($content);
+
 include('V_footer.php');
 
  ?>
