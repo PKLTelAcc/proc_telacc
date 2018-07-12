adcfbffa27ea304ba60b7d3855dabfb15ef3917b
unknown <bagusdevil54@gmail.com>
2018-07-12 08:38:28 +0700

    mitra

---
+++ application/controllers/C_mitra.php
@@ -0,0 +1,64 @@
+<?php
+/**
+* 
+*/
+class C_mitra extends CI_Controller
+{
+	
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('M_mitra');
+	}
+
+	public function index()
+	{
+
+		$mitra=$this->M_mitra->view();
+		$data = array(
+			'title'=>'Input mitra',
+			'content' => 'V_mitra',
+			'mitra' =>$mitra
+		);
+		$this->load->view('tampilan/V_combine',$data);
+
+	}
+	public function form()
+	{
+		$nama = $_POST['txtNama'];
+
+
+		$data = array(
+			'MTRA_NAME' =>$nama
+			);
+		$mitra=$this->M_mitra->insert($data);
+		 redirect('C_mitra');
+	}
+	public function formUpdate($id)
+	{
+		$mitra=$this->M_mitra->update($id);
+		$data = array(
+			'content'=>'v_editMitra',
+			'title'=>'Edit Mitra',
+			'mitra' =>$mitra
+		);
+		$this->load->view('tampilan/v_combine',$data);
+	}
+	public function updateData($id)
+	{
+		$nama = $_POST['txtNama'];
+
+
+		$data = array(
+			'MTRA_NAME' =>$nama
+			);
+		$mitra=$this->M_mitra->updateData($id, $data);
+		 redirect('C_mitra');
+	}
+	public function delete($id)
+	{
+		$this->db->delete('mitra', array('MTRA_ID' => $id));
+		redirect('C_mitra');
+	}
+}
+?>
