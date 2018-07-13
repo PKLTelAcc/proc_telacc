e6e4bbff12efe5f27ab427ca7152a82b14bbc629
unknown <bagusdevil54@gmail.com>
2018-07-13 10:04:50 +0700

    mitra

--- application/views/V_mitra.php
+++ application/views/V_mitra.php
@@ -1,34 +1,94 @@
-<form action="<?php echo base_url(). 'C_mitra/form'; ?>" method="POST">
-	<h3>Input Mitra</h3>
-	<label>Nama Mitra</label>
-	<input type="text" name="txtNama" required="true">
-	<button type="reset">Cancel</button>
-	<button type="submit">Input Data</button>
-</form>
+ <!-- Main content -->
+<div class="content">
+	<div class="row">
+	  		<div class="col-md-12">
+			    <div class="box box-info">
+			      <div class="box-header with-border">
+			        <h3 class="box-title">Input Mitra</h3>
 
-<hr>
+			        <div class="box-tools pull-right">
+			          <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+			        </div>
+			      </div>
+			      <!-- /.box-header -->
+			      <div class="box-body">
+			        <div class="row">
+			          <div class="col-md-12 ">
+			            <form action="<?php echo base_url().'C_mitra/form'; ?>" method="POST">
+	                      <div class="form-group">
+			                  <label class=" control-label">Input Mitra</label>
+			                  <div>
+			                    <span >
+			                      <input class="form-control" type="text" placeholder="Mitra" id="mitra" name="txtNama">  
+			                    </span>
+			                  </div>
+			              </div>
+			              <div class="form-group">
+			                  <div class="row">
+			                  	<div class="col-md-1">
+				                    <button type="reset" class="btn btn-default pull-right">Cancel</button>
+				                  </div>
+				                  <div class="col-md-1">
+				                    <button type="submit" class="btn btn-info pull-right">Input Data</button>
+				                  </div>
+			                  </div>
+			              </div>
+			            </form>
+			          </div>
+			          <!-- /.col -->
+			        </div>
+			        <!-- /.row -->
+			      </div>
+			    </div>
+			      <!-- /.box -->
+			  </div> <!-- col-input -->
+<!--'<div class="col-md-6">'; (itu di atas di echo) -->		
+	    }
+	    <div class="box box-info">
+	      <div class="box-header with-border">
+	        <h3 class="box-title">Data Mitra</h3>
 
-<h3>Data Mitra</h3>
-<table border="1">
-	<thead>
-		<tr>
-			<th>No.</th>
-			<th>Nama Mitra</th>
-			<th>Action</th>
-		</tr>
-	</thead>
-	<tbody>
-		<?php 
-			$no=1;
-			foreach ($mitra as $row) {
-				echo "<tr>";
-				echo "<td>".$no."</td>";
-				echo "<td>".$row['MTRA_NAME']."</td>";
-				echo "<td><a href='".base_url()."C_mitra/FormUpdate/".$row['MTRA_ID']."'>Edit</a> | <a href='".base_url()."C_mitra/delete/".$row['MTRA_ID']."'>Delete</a></td>";
-				echo "</tr>";
-
-				$no++;
-			}
-		 ?>
-	</tbody>
-</table>
+	        <div class="box-tools pull-right">
+	          <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+	        </div>
+	      </div>
+	      <!-- /.box-header -->
+	      <div class="box-body">
+			<div class="col-md-12">
+				<table class="table table-bordered table-hover table-striped" id="urut">
+					<thead>
+						<tr>
+							<th>no</th>
+							<th>Nama Mita</th>>
+							<th style="text-align: center" >Action </th>
+						</tr>
+					</thead>
+					<tbody>
+					 <tbody>
+		 			  	<tr>
+		 			  		<?php
+		 			  		$no=1;
+		 			  		foreach ($mitra as $row) {
+		 			  			?>
+		 			  		<td><?php echo $no; ?></td>
+		 			  		<td><?php echo $row['MTRA_NAME']; ?></td>
+					 			 <td class="center">
+					 			  <a href="<?php echo base_url().'C_mitra/delete/'.$row['MTRA_ID']; ?>">Delete</a>
+					 			  |
+					 			  <a href="<?php echo base_url().'C_mitra/FormUpdate/'.$row['MTRA_ID']; ?>"> Edit</a>
+					 			 </td>
+		 			  	</tr>
+		 			 <?php
+		 			 $no++; 
+		 			  	}
+		 			  ?>
+					</tbody>
+				</table>
+			</div>
+	      </div>
+	    </div>
+	      <!-- /.box -->
+	  </div> <!-- col-input -->
+	</div>
+</div>
+<!-- /.content -->
