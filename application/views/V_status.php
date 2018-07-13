da4d3664313915daf9589b791fe24af41c2be05c
unknown <bagusdevil54@gmail.com>
2018-07-13 13:29:25 +0700

    status

--- application/views/V_status.php
+++ application/views/V_status.php
@@ -1,34 +1,85 @@
-<form action="<?php echo base_url(). 'C_status/form'; ?>" method="POST">
-	<h3>Input Status</h3>
-	<label>Nama Status</label>
-	<input type="text" name="txtNama" required="true">
-	<button type="reset">Cancel</button>
-	<button type="submit">Input Data</button>
-</form>
+<!-- Main content -->
+<div class="content">
+	<div class="row">
+	  <div class="col-md-12">
+	    <div class="box box-info">
+	      <div class="box-header with-border">
+	        <h3 class="box-title">Input Status</h3>
 
-<hr>
+	        <div class="box-tools pull-right">
+	          <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+	        </div>
+	      </div>
+	      <!-- /.box-header -->
+	      <div class="box-body">
+	        <div class="row">
+	          <div class="col-md-12 ">
+	            <form action="<?php echo base_url(). 'C_status/form'; ?>" method="POST">
+	              <div class="form-group">
+	                  <label class=" control-label">Status</label>
+	                  <div>
+	                    <span id="qty">
+	                      <input class="form-control" type="text" placeholder="Level .." name="txtNama" required placeholder="0">  
+	                    </span>
+	                  </div>
+	              </div>
+	              <div class="form-group">
+	                <div class="row">
+	                  <div class="col-md-10">
+	                    <button type="reset" class="btn btn-default pull-right">Cancel</button>
+	                  </div>
+	                  <div class="col-md-2">
+	                    <button type="submit" class="btn btn-info pull-right" data-toggle="modal" data-target="#modal-success2" onclick="modalKonfirmasiTakJadi()" >Input status</button>
+	                  </div>
+	                </div>
+	              </div>
+	            </form>
+	          </div>
+	          <!-- /.col -->
+	        </div>
+	        <!-- /.row -->
+	      </div>
+	    </div>
+	      <!-- /.box -->
+	  </div> <!-- col-input -->
+	  <div class="col-md-12">
+	    <div class="box box-info">
+	      <div class="box-header with-border">
+	        <h3 class="box-title">Data Status</h3>
 
-<h3>Data Status</h3>
-<table border="1">
-	<thead>
-		<tr>
-			<th>No.</th>
-			<th>Nama Status</th>
-			<th>Action</th>
-		</tr>
-	</thead>
-	<tbody>
-		<?php 
-			$no=1;
-			foreach ($status as $row) {
-				echo "<tr>";
-				echo "<td>".$no."</td>";
-				echo "<td>".$row['STAT_NAME']."</td>";
-				echo "<td><a href='".base_url()."C_status/FormUpdate/".$row['STAT_ID']."'>Edit</a> | <a href='".base_url()."C_status/delete/".$row['STAT_ID']."'>Delete</a></td>";
-				echo "</tr>";
-
-				$no++;
-			}
-		 ?>
-	</tbody>
-</table>
+	        <div class="box-tools pull-right">
+	          <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+	        </div>
+	      </div>
+	      <!-- /.box-header -->
+	      <div class="box-body">
+			<table class="table table-bordered table-hover table-striped" id="lookup">
+				<thead>
+					<tr>
+						<th>No.</th>
+						<th>status</th>
+						<th style="text-align: center">Action </th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+					$no=1; 
+		 				foreach ($status as $row) {
+		 					echo "<tr>";
+		 					echo "<td>".$no."</td>";
+		 					echo "<td>".$row['STAT_NAME']."</td>";
+		 					echo "<td><a href='".base_url()."C_status/FormUpdate/".$row['STAT_ID']."'>Edit</a> | 
+		 						<a href='".base_url()."C_status/delete/".$row['STAT_ID']."'>Delete</a></td>";
+		 					echo "</tr>";
+		 					$no++;
+		 				}
+		 			 ?>
+				</tbody>
+			</table>
+	      </div>
+	    </div>
+	      <!-- /.box -->
+	  </div> <!-- col-input -->
+	</div>
+</div>
+<!-- /.content -->
