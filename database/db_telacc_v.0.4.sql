7fe1178bdb84ca499249f15246864faaf4f61a4f
hanifmt <hanifmtsabit@gmail.com>
2018-07-16 08:12:48 +0700

    update

---
+++ database/db_telacc_v.0.4.sql
@@ -0,0 +1,321 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 16, 2018 at 03:09 AM
+-- Server version: 10.1.34-MariaDB
+-- PHP Version: 7.2.7
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_telacc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `instalasi`
+--
+
+CREATE TABLE `instalasi` (
+  `INST_ID` int(11) NOT NULL,
+  `INST_MATERIAL` int(11) NOT NULL,
+  `INST_JASA` int(11) NOT NULL,
+  `INST_TOTAL` int(11) NOT NULL,
+  `INST_ODP` int(11) NOT NULL,
+  `INST_PROGRES` text NOT NULL,
+  `INST_KENDALA` text NOT NULL,
+  `INST_DOKUMENTASI` text NOT NULL,
+  `INST_TANGGAL` date NOT NULL,
+  `INST_TARGET` date NOT NULL,
+  `INST_TIMESTAMP` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `INST_WODE_ID` int(11) NOT NULL,
+  `INST_MITA_ID` int(11) NOT NULL,
+  `INST_PEGA_ID` int(11) NOT NULL,
+  `INST_WTEL_ID` int(11) NOT NULL,
+  `INST_SWIT_ID` int(11) NOT NULL,
+  `INST_PROG_ID` int(11) NOT NULL,
+  `INST_STAT_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `level`
+--
+
+CREATE TABLE `level` (
+  `LEVE_ID` int(11) NOT NULL,
+  `LEVE_NAME` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mitra`
+--
+
+CREATE TABLE `mitra` (
+  `MTRA_ID` int(11) NOT NULL,
+  `MTRA_NAME` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pegawai`
+--
+
+CREATE TABLE `pegawai` (
+  `PEGA_ID` int(11) NOT NULL,
+  `PEGA_NAME` varchar(100) NOT NULL,
+  `PEGA_NIK` int(11) NOT NULL,
+  `PEGA_PSA` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `program`
+--
+
+CREATE TABLE `program` (
+  `PROG_ID` int(11) NOT NULL,
+  `PROG_NAME` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `status`
+--
+
+CREATE TABLE `status` (
+  `STAT_ID` int(11) NOT NULL,
+  `STAT_NAME` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sub_witel`
+--
+
+CREATE TABLE `sub_witel` (
+  `SWIT_ID` int(11) NOT NULL,
+  `SWIT_NAME` varchar(100) NOT NULL,
+  `SWIT_WTEL_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `survey`
+--
+
+CREATE TABLE `survey` (
+  `SURV_ID` int(11) NOT NULL,
+  `SURV_MATERIAL` int(11) NOT NULL,
+  `SURV_JASA` int(11) NOT NULL,
+  `SURV_TOTAL` int(11) NOT NULL,
+  `SURV_ODP` int(11) NOT NULL,
+  `SURV_TANGGAL` date NOT NULL,
+  `SURV_TIMESTAMP` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `SURV_WODE_ID` int(11) NOT NULL,
+  `SURV_PEGA_ID` int(11) NOT NULL,
+  `SURV_WTEL_ID` int(11) NOT NULL,
+  `SURV_SWIT_ID` int(11) NOT NULL,
+  `SURV_PROG_ID` int(11) NOT NULL,
+  `SURV_STAT_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `USER_ID` int(11) NOT NULL,
+  `USER_NAME` varchar(50) NOT NULL,
+  `USER_PASSWORD` varchar(50) NOT NULL,
+  `USER_PICTURE` text NOT NULL,
+  `USER_TIMESTAMP` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `USER_LEVE_ID` int(11) NOT NULL,
+  `USER_PEGA_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `witel`
+--
+
+CREATE TABLE `witel` (
+  `WTEL_ID` int(11) NOT NULL,
+  `WTEL_NAME` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `work_order`
+--
+
+CREATE TABLE `work_order` (
+  `WODE_ID` int(11) NOT NULL,
+  `WODE_ID_TA` char(11) NOT NULL,
+  `WODE_NAMA_LOKASI` text NOT NULL,
+  `WODE_KOORDINAT` text NOT NULL,
+  `WODE_ALAMAT` text NOT NULL,
+  `WODE_TANGGAL` date NOT NULL,
+  `WODE_TIMESTAMP` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `WODE_WTEL_ID` int(11) NOT NULL,
+  `WODE_SWIT_ID` int(11) NOT NULL,
+  `WODE_PROG_ID` int(11) NOT NULL,
+  `WODE_STAT_ID` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `instalasi`
+--
+ALTER TABLE `instalasi`
+  ADD PRIMARY KEY (`INST_ID`);
+
+--
+-- Indexes for table `mitra`
+--
+ALTER TABLE `mitra`
+  ADD PRIMARY KEY (`MTRA_ID`);
+
+--
+-- Indexes for table `pegawai`
+--
+ALTER TABLE `pegawai`
+  ADD PRIMARY KEY (`PEGA_ID`);
+
+--
+-- Indexes for table `program`
+--
+ALTER TABLE `program`
+  ADD PRIMARY KEY (`PROG_ID`);
+
+--
+-- Indexes for table `status`
+--
+ALTER TABLE `status`
+  ADD PRIMARY KEY (`STAT_ID`);
+
+--
+-- Indexes for table `sub_witel`
+--
+ALTER TABLE `sub_witel`
+  ADD PRIMARY KEY (`SWIT_ID`);
+
+--
+-- Indexes for table `survey`
+--
+ALTER TABLE `survey`
+  ADD PRIMARY KEY (`SURV_ID`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`USER_ID`);
+
+--
+-- Indexes for table `witel`
+--
+ALTER TABLE `witel`
+  ADD PRIMARY KEY (`WTEL_ID`);
+
+--
+-- Indexes for table `work_order`
+--
+ALTER TABLE `work_order`
+  ADD PRIMARY KEY (`WODE_ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `instalasi`
+--
+ALTER TABLE `instalasi`
+  MODIFY `INST_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `mitra`
+--
+ALTER TABLE `mitra`
+  MODIFY `MTRA_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `pegawai`
+--
+ALTER TABLE `pegawai`
+  MODIFY `PEGA_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `program`
+--
+ALTER TABLE `program`
+  MODIFY `PROG_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `status`
+--
+ALTER TABLE `status`
+  MODIFY `STAT_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `sub_witel`
+--
+ALTER TABLE `sub_witel`
+  MODIFY `SWIT_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `survey`
+--
+ALTER TABLE `survey`
+  MODIFY `SURV_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `USER_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `witel`
+--
+ALTER TABLE `witel`
+  MODIFY `WTEL_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `work_order`
+--
+ALTER TABLE `work_order`
+  MODIFY `WODE_ID` int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
