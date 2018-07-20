c2b6c0113006755ea8c9f2eb0bf2db56e6888f77
hanifmt <hanifmtsabit@gmail.com>
2018-07-20 09:15:24 +0700

    update

--- application/models/M_instalasi.php
+++ application/models/M_instalasi.php
@@ -18,7 +18,7 @@ class M_instalasi extends CI_Model
 	}
 	public function getWorkOrder()
 	{
-		$sql = "SELECT * FROM work_order INNER JOIN witel ON WODE_WTEL_ID = WTEL_ID INNER JOIN sub_witel ON WODE_SWIT_ID = SWIT_ID INNER JOIN program ON WODE_PROG_ID = PROG_ID INNER JOIN status ON WODE_STAT_ID = STAT_ID";
+		$sql = "SELECT * FROM work_order INNER JOIN witel ON WODE_WTEL_ID = WTEL_ID INNER JOIN sub_witel ON WODE_SWIT_ID = SWIT_ID INNER JOIN program ON WODE_PROG_ID = PROG_ID INNER JOIN status ON WODE_STAT_ID = STAT_ID INNER JOIN survey ON WODE_ID = SURV_WODE_ID";
 		$query=$this->db->query($sql);
 		$return = $query->result_array();
 		return $return;
