895a10c270c84199c78210dc60efc746ef294658
hanifmt <hanifmtsabit@gmail.com>
2018-07-31 08:52:52 +0700

    update

--- application/controllers/C_dashboard.php
+++ application/controllers/C_dashboard.php
@@ -31,10 +31,11 @@ class C_dashboard extends CI_Controller
 	public function detailWitel($id)
 	{
 		$dataReportByWitel		= $this->M_dashboard->getReportByWitelId($id);
+		date_default_timezone_set("Asia/Bangkok");
 		$data = array(
 			'dataReportByWitel' 	=> $dataReportByWitel,
 			'content' 				=> 'V_reportDetailWitel',
-			'title'					=> 'Detail Witel '.$dataReportByWitel[0]['WTEL_NAME'],
+			'title'					=> 'Detail Witel '.$dataReportByWitel[0]['WTEL_NAME'].' | '.date("l jS \of F Y h:i A"),
 		);
 		$this->load->view('tampilan/v_combine',$data);	
 	}
@@ -42,10 +43,11 @@ class C_dashboard extends CI_Controller
 	public function detailStatus($id)
 	{
 		$dataReportByStatus		= $this->M_dashboard->getReportByStatusId($id);
+		date_default_timezone_set("Asia/Bangkok");
 		$data = array(
 			'dataReportByStatus' 	=> $dataReportByStatus,
 			'content' 				=> 'V_reportDetailStatus',
-			'title'					=> 'Detail Status '.$dataReportByStatus[0]['STAT_NAME'],
+			'title'					=> 'Detail Status '.$dataReportByStatus[0]['STAT_NAME'].' | '.date("l jS \of F Y h:i A"),
 		);
 		$this->load->view('tampilan/v_combine',$data);	
 	}
@@ -53,10 +55,11 @@ class C_dashboard extends CI_Controller
 	public function detail($wtelid,$statid)
 	{
 		$dataReportByWitelStatus		= $this->M_dashboard->getReportByWitelStatus($wtelid,$statid);
+		date_default_timezone_set("Asia/Bangkok");
 		$data = array(
 			'dataReportByWitelStatus' 		=> $dataReportByWitelStatus,
 			'content' 				=> 'V_reportDetailWitelStatus',
-			'title'					=> 'Detail Witel '.$dataReportByWitelStatus[0]['WTEL_NAME'].' Status '.$dataReportByWitelStatus[0]['STAT_NAME'],
+			'title'					=> 'Detail Witel '.$dataReportByWitelStatus[0]['WTEL_NAME'].' Status '.$dataReportByWitelStatus[0]['STAT_NAME'].' | '.date("l jS \of F Y h:i A"),
 		);
 		$this->load->view('tampilan/v_combine',$data);	
 	} 
